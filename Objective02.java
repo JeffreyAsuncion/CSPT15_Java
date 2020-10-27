fadadced85bf6401fb87356f79a753e4584391f6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-26 22:05:59 -0400

    Objective02 Hello, World

---
+++ Objective02.java
@@ -0,0 +1,9 @@
+public class Objective02 {
+
+    public static void main(String[] args) {
+        
+	// print the line "Hello, World!"
+        System.out.println("Hello, World!");
+    
+    }
+}
