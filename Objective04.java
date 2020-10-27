81c1af73c5882f094b9449f1a816ed4861344ed8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-26 23:10:28 -0400

    Objective04 Typecasting in Java

---
+++ Objective04.java
@@ -0,0 +1,29 @@
+public class Objective04 {
+
+    public static void main(String[] args) {
+    
+        /* Modify the assignment statements below so that 
+	 * an integer is assigned to "my_int",
+	 * a floating point is assigned to "my_float",
+	 * and a string is assigned to "my_string".
+	 *
+	 * Then, Use Typecasting to print your integer as floating point.
+	 * Use typecasting again to print your floating point as integer.
+	 */
+        
+        // Modify the code below to fulfill the requirements outlined in the docstring.
+	int my_int = 10;
+	double my_float = 3.14;
+	String my_string = "I'm a String!";
+
+	// Practice typecasting as outlined above with these print statements.
+	double int_to_float = my_int; 	// Automatic casting: int to float
+	int double_to_int = (int) my_float;  // Manual casting : float to int
+
+	System.out.println("to float : " + int_to_float);
+	System.out.println("  to int : " + double_to_int);
+	System.out.println(" String  : " + my_string);
+
+
+    }
+}
