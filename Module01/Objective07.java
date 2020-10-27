a30115737c4ae3b4c8bab9e7812da2fd8d18f42f
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-27 01:12:48 -0400

    Objective07 - Create formatted strings

---
+++ Module01/Objective07.java
@@ -0,0 +1,24 @@
+// Objective 07 - Create formatted strings
+
+
+public class Objective07 {
+
+    public static void main(String[] args) {
+    
+        /* 1. Assign three different types of data to the variables "a", "b", "c".
+	 *
+	 * 2. Use a format string to injet the data from three variables into the string.
+	 */
+	
+	// Modify the code below to meet the requirements above
+	
+	int a = 10;
+	double b = 3.14159254;
+	String c = "Rock Steady Crew";
+
+	System.out.format("a: %d,  b: %f, c: %s\n", a, b, c);
+
+
+	
+    }
+}
