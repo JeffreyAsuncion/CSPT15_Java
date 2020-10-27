2174f6a01d55f041991a5fbdfe6222d90faf4613
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-26 22:13:26 -0400

    Objective03 - Use white space to denote blocks

---
+++ Objective03.java
@@ -0,0 +1,12 @@
+public class Objective03 {
+
+    public static void main(String[] args) {
+    
+        String school = "Lambda";
+
+	if (school == "Lambda") {
+	    System.out.println("Unlocking potential, regardless of circumstance.");
+	}
+    }
+}
+
